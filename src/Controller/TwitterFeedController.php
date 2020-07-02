6c4458ba78b9acc32930861d557a0b2019d2a32f
Jamie <jamie@supercooldesign.co.uk>
2020-07-03 00:01:36 +0100

    Finished implementing TwitterAuth service

--- src/Controller/TwitterFeedController.php
+++ src/Controller/TwitterFeedController.php
@@ -4,7 +4,9 @@ namespace App\Controller;
 
 use App\Service\TwitterAuth;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class TwitterFeedController extends AbstractController
 {
@@ -13,8 +15,26 @@ class TwitterFeedController extends AbstractController
      */
     public function index(TwitterAuth $twitter)
     {
+        //name of twitter user
+        $name = "elon";
+        $result = $twitter->timeline($name);
+
+        dd($result);
+
         return $this->render('twitter_feed/index.html.twig', [
             'controller_name' => 'TwitterFeedController',
         ]);
     }
+
+    /**
+     * example request
+     *
+     * This will get the timeline of the user specified
+     *
+     * $name = "<sceen_name of user";
+     * $screenName="<name of person>";
+     *
+     * $twitter->timeline(name);
+     *
+     */
 }
